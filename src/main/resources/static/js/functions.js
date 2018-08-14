24b3fabb42d45f2add2627bc6b80c693f9f1f3b6
Valentin Chiriac <valentinchiriac@Valentins-MacBook.local>
2018-08-14 07:45:45 +0200

    Initial commit

---
+++ src/main/resources/static/js/functions.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function() {
+
+    // Replace the validation UI for all forms
+    //var forms = $('#validationForm')[0];
+/*    $('.important').change(function() {
+        if(($(this).val()) !== '') $(this).css( 'border-width', '0' );
+        else  $(this).css( 'border-width', '2' );
+    });*/
+
+    $('#validationForm').on('submit',function(){
+        var element = $('#title');
+        var name = $.trim(element.val());
+        if (name === '') {
+            var parent = element[0].parentElement;
+            show_error(parent, "This is the error");
+            //$('#title')[0].setCustomValidity('my custom error');
+            event.preventDefault();
+        }
+    });
+
+/*    $('#sendbutton').on('click', function (event) {
+
+    });*/
+
+
+
+
+
+});
+
+function getText() {
+    return 'test';
+}
+
+function show_error(parent, message) {
+    parent.insertAdjacentHTML("beforeend", "<span class='error_bubble'>" + message + "</span>");
+}
